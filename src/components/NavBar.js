aec07baeafcd3b0de936961dda00675f3f095c6f
Seo Choi <seochoi56@gmail.com>
2022-09-22 20:41:16 -0400

    commit tree 343c10e2f934081f960120d5e4e49fe73fdd739a

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+
+
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const showLinks = links.map((link) => {
+    let hashLink = "#" + link
+    return <a key={link} href={hashLink}>{link}</a>
+  })
+  return <nav>{showLinks}</nav>;
 }
 
 export default NavBar;
